54ed35458f3c219769ab48d9fcfdbb2af8ac9a96
KovalevAlexey87 <alexey_kovalev87@mail.ru>
2022-04-20 10:52:15 +0300

    lesson01-6 доработка

--- lesson1_6.py
+++ lesson1_6.py
@@ -20,9 +20,7 @@
 a = int(input("Введите результаты пробежки первого дня в км "))
 b = int(input("Введите общий желаемый результат в км "))
 result_days = 1
-result_km = a
-while result_km < b:
-        a = a + 0.1 * a
-        result_days += 1
-        result_km = result_km + a
-print(f"Вы достигнете требуемых показателей на %.d день" % result_days)
+while a < b:
+    a *= 1.1
+    result_days += 1
+print(f"Ответ: на {result_days}-й день спортсмен достиг результата — не менее {b} км.")
